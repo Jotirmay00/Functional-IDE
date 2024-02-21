9ce801c17f699d0e8e3996d3be1ae40326d01507
Jotirmay Manik <116794918+Jotirmay00@users.noreply.github.com>
2024-02-21 21:52:06 +0530

    Update settings.py

--- IDE/settings.py
+++ IDE/settings.py
@@ -81,7 +81,7 @@ STATICFILES_DIRS = [
     os.path.join(DIST_DIR, 'assets'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 WSGI_APPLICATION = 'IDE.wsgi.application'
